edb2b35a7b2e38e8e907a25b723693a668d39e13
houlin.jiang <houlin.jiang@qunar.com>
2016-04-25 19:47:02 +0800

    first commit

---
+++ src/com/holy/createPattern/factoryPattern/Computer.java
@@ -0,0 +1,11 @@
+package com.holy.createPattern.factoryPattern;
+
+/**
+ * Created by houlin.jiang on 16-4-25.
+ */
+public class Computer implements Factory{
+    @Override
+    public void create() {
+        System.out.println("I am Computer Product ! Create an Computer");
+    }
+}
---
+++ src/com/holy/createPattern/singletonPatter/HugrySingleton.java
@@ -0,0 +1,16 @@
+package com.holy.createPattern.singletonPatter;
+
+/**
+ * Created by houlin.jiang on 16-4-25.
+ */
+public class HugrySingleton {
+    private static HugrySingleton mHugrySingleton = new HugrySingleton();
+
+    public static HugrySingleton getInstance() {
+        return mHugrySingleton;
+    }
+
+    public void showMessage(){
+        System.out.println("Hello World!");
+    }
+}
